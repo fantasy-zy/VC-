9aebd2284477ac637dc6822d35ebd3be710f0ce9
ZhangY <983662456@qq.com>
2023-12-23 11:29:29 +0800

    添加项目文件。

---
+++ Draw/Draw.h
@@ -0,0 +1,32 @@
+﻿
+// Draw.h: Draw 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CDrawApp:
+// 有关此类的实现，请参阅 Draw.cpp
+//
+
+class CDrawApp : public CWinApp
+{
+public:
+	CDrawApp() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDrawApp theApp;
---
+++ Draw/DrawView.cpp
@@ -0,0 +1,82 @@
+﻿
+// DrawView.cpp: CDrawView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Draw.h"
+#endif
+
+#include "DrawDoc.h"
+#include "DrawView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDrawView
+
+IMPLEMENT_DYNCREATE(CDrawView, CView)
+
+BEGIN_MESSAGE_MAP(CDrawView, CView)
+END_MESSAGE_MAP()
+
+// CDrawView 构造/析构
+
+CDrawView::CDrawView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CDrawView::~CDrawView()
+{
+}
+
+BOOL CDrawView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDrawView 绘图
+
+void CDrawView::OnDraw(CDC* /*pDC*/)
+{
+	CDrawDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CDrawView 诊断
+
+#ifdef _DEBUG
+void CDrawView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDrawView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDrawDoc* CDrawView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDrawDoc)));
+	return (CDrawDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDrawView 消息处理程序
